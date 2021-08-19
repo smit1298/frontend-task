45016fdceaf8ac8443bea1967ea81fdc1e1f7839
smit1298 <61621188+smit1298@users.noreply.github.com>
2021-08-19 21:14:51 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Application from './components/Application';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Application />
     </div>
   );
 }
